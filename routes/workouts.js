b0cf58c5201b71ab55ec6d1d4e9af2ebd54d8a2f
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-09-09 23:14:24 +0530

    Update workouts routes

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,7 +1,27 @@
 const express = require('express')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout,
+} = require('../controllers/WorkoutController')
 
 const router = express.Router()
 
-router.get('/', () => {})
+// GET all workouts
+router.get('/', getWorkouts)
+
+// GET a single workout
+router.get('/:id', getWorkout)
+
+// POST a new workout
+router.post('/', createWorkout)
+
+// DELETE a workout
+router.delete('/:id', deleteWorkout)
+
+// UPDATE a workout
+router.patch('/:id', updateWorkout)
 
 module.exports = router
